e722cd8dad00b6fe30b0ad8827222adb13b9d840
Seungjin Jeong <awa1530@naver.com>
2024-10-15 16:41:31 +0900

    실습 3 제출합니다
    실습 3 제출합니다.

---
+++ practice3/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2, result = 0;
+	char operations;
+	printf("수식을 입력하시오:\n");
+	scanf_s("%d %c %d", &num1, &operations, sizeof(operations), &num2);
+
+	switch (operations)
+	{
+	case '+' :
+		result = num1 + num2;
+		break;
+		
+	case '-' :
+		result = num1 - num2;
+		break;
+
+	case '*' :
+		result = num1 * num2;
+		break;
+		
+	case '/' :
+		result = num1 / num2;
+		break;
+	default :
+		printf("잘못된 수식입니다.\n");
+	}
+
+	printf("계산결과 : %d\n", result);
+	return 0;
+}
