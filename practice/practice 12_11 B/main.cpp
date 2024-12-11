2c92e890fe53d0becab50174013c60856c9228b2
Seungjin Jeong <awa1530@naver.com>
2024-12-11 18:43:51 +0900

    실습
    실습

--- practice/practice 12_11 B/main.cpp
+++ practice/practice 12_11 B/main.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-void calculateStats(int* array, int size, double* sum, double* mean, double* variance, double* std_dev) 
+void CStats(int* array, int size, double* sum, double* mean, double* variance, double* std_dev) 
 {
     *sum = 0;
     *variance = 0;
@@ -24,27 +24,28 @@ void calculateStats(int* array, int size, double* sum, double* mean, double* var
 
 int main(void) 
 {
-    int initialSize, additionalSize;
+    int inSize, addSize;
     int* array;
 
     printf("초기 배열 크기: ");
-    scanf_s("%d", &initialSize);
+    scanf_s("%d", &inSize);
 
-    array = (int*)malloc(initialSize * sizeof(int));
+    array = (int*)malloc(inSize * sizeof(int));
     if (array == NULL) 
     {
         printf("메모리 실패\n");
         return 1;
     }
 
-    printf("입력된 값: ", initialSize);
-    for (int i = 0; i < initialSize; i++) 
+    printf("입력된 값: ", inSize);
+    for (int i = 0; i < inSize; i++)
     {
         scanf_s("%d", &array[i]);
     }
 
     double sum, mean, variance, std_dev;
-    calculateStats(array, initialSize, &sum, &mean, &variance, &std_dev);
+    
+    CStats(array, inSize, &sum, &mean, &variance, &std_dev);
 
     printf("합계: %.2lf\n", sum);
     printf("평균: %.2lf\n", mean);
@@ -52,22 +53,22 @@ int main(void)
     printf("표준편차: %.2lf\n", std_dev);
 
     printf("추가 배열 크기: ");
-    scanf_s("%d", &additionalSize);
+    scanf_s("%d", &addSize);
 
-    array = (int*)realloc(array, (static_cast<unsigned long long>(initialSize) + additionalSize) * sizeof(int));
+    array = (int*)realloc(array, (static_cast < unsigned long long > (inSize) + addSize) * sizeof(int));
     if (array == NULL) 
     {
         printf("메모리 실패\n");
         return 1;
     }
 
-    printf("추가된 값: ", additionalSize);
-    for (int i = initialSize; i < initialSize + additionalSize; i++)
+    printf("추가된 값: ", addSize);
+    for (int i = inSize; i < inSize + addSize; i++)
     {
         scanf_s("%d", &array[i]);
     }
 
-    calculateStats(array, initialSize + additionalSize, &sum, &mean, &variance, &std_dev);
+    CStats(array, inSize + addSize, &sum, &mean, &variance, &std_dev);
 
     printf("새로운 합계: %.2lf\n", sum);
     printf("새로운 평균: %.2lf\n", mean);
